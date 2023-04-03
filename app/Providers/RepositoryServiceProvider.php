7d264fc2ec5c0092c3faa830d3d8c829cd57ddf4
Gabriel Santos Meireles <dev.meireles@gmail.com>
2023-04-03 17:11:16 +0100

    adding requirement module

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -7,10 +7,12 @@ use Illuminate\Support\ServiceProvider;
 use App\Interfaces\CuisineInterface;
 use App\Interfaces\IncludesInterface;
 use App\Interfaces\TagInterface;
+use App\Interfaces\RequirementInterface;
 
 use App\Repositories\CuisineRepository;
 use App\Repositories\IncludesRepository;
 use App\Repositories\TagRepository;
+use App\Repositories\RequirementRepository;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -22,6 +24,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(CuisineInterface::class, CuisineRepository::class);
         $this->app->bind(IncludesInterface::class, IncludesRepository::class);
         $this->app->bind(TagInterface::class, TagRepository::class);
+        $this->app->bind(RequirementInterface::class, RequirementRepository::class);
     }
 
     /**
